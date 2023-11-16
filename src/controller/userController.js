4221f39540a48356b09a7ecb31b58a5c5eefebf4
PhongVan94 <vanbv94@gmail.com>
2023-11-16 20:48:29 +0700

    add CRUD API  and pagination

---
+++ src/controller/userController.js
@@ -0,0 +1,73 @@
+import userApiService from '../services/userApiService'
+
+
+const readFunc = async (req, res) => {
+    try {
+        if (req.query.page && req.query.limit) {
+            let page = req.query.page;
+            let limit = req.query.limit;
+            let data = await userApiService.getUserWithPagination(+page, +limit);
+            return res.status(200).json({
+                EM: data.EM,
+                EC: data.EC,
+                DT: data.DT
+            })
+        } else {
+            let data = await userApiService.getAllUser();
+            return res.status(200).json({
+                EM: data.EM,
+                EC: data.EC,
+                DT: data.DT
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const createFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const updateFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const deleteFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+module.exports = {
+    createFunc, readFunc, updateFunc, deleteFunc
+}
---
+++ src/services/userApiService.js
@@ -0,0 +1,121 @@
+import db from "../models";
+
+const getAllUser = async () => {
+    try {
+        let users = await db.User.findAll({
+            attributes: ["id", "username", "email", "phone", "sex"],
+            include: { model: db.Group, attributes: ["name", "description"], }
+        });
+        if (users) {
+            console.log(users)
+            return {
+                EM: 'GET DATA SUCCESS',
+                EC: 0,
+                DT: users
+            }
+        } else {
+            return {
+                EM: 'GET SUCCESS',
+                EC: 0,
+                DT: []
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'SOMETHING WENT WRONGS',
+            EC: -1,
+            DT: []
+        }
+    }
+}
+
+const getUserWithPagination = async (page, limit) => {
+    try {
+        let offset = (page - 1) * limit;
+
+        const { count, rows } = await db.User.findAndCountAll({
+            offset: offset
+            , limit: limit,
+        })
+
+        let totalPages = Math.ceil(count / limit);
+        let data = {
+            totalRows: count,
+            totalPages: totalPages,
+            users: rows
+        }
+        console.log(data);
+        return {
+            EM: 'GET DATA SUCCESS',
+            EC: 0,
+            DT: data
+        }
+
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'SOMETHING WENT WRONGS',
+            EC: -1,
+            DT: []
+        }
+    }
+}
+
+const createNewUser = async (data) => {
+    try {
+        await db.User.create({
+
+        })
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'SOMETHING WENT WRONGS',
+            EC: -1,
+            DT: []
+        }
+    }
+
+}
+const updateUser = async (data) => {
+    try {
+        let user = await db.User.findOne({
+            where: { id: data.id }
+        })
+        if (user) {
+            //update
+            user.save({
+
+            })
+        } else {
+            //not found
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'SOMETHING WENT WRONGS',
+            EC: -1,
+            DT: []
+        }
+    }
+
+}
+const deleteUser = async (id) => {
+    try {
+        await db.User.delete({
+            where: { id: id }
+        })
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'SOMETHING WENT WRONGS',
+            EC: -1,
+            DT: []
+        }
+    }
+
+}
+
+module.exports = {
+    getAllUser, createNewUser, updateUser, deleteUser, getUserWithPagination
+}
