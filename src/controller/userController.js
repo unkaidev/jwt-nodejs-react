24b0f2aca75e0677b9e4ed799936eb6230633e5f
PhongVan94 <vanbv94@gmail.com>
2023-11-20 10:03:11 +0700

    delete User

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -56,9 +56,14 @@ const updateFunc = (req, res) => {
     }
 }
 
-const deleteFunc = (req, res) => {
+const deleteFunc = async (req, res) => {
     try {
-
+        let data = await userApiService.deleteUser(req.body.id);
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
     } catch (error) {
         console.log(error)
         return res.status(500).json({
